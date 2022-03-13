c63f880329e09761cbfbe357c0df43c451aed48d
Jeremy Brooks <jeremydanielbrooks@yahoo.com>
2022-03-12 20:59:48 -0600

    desc change test

--- R/load_rosters.R
+++ R/load_rosters.R
@@ -1,4 +1,4 @@
-# load roster data
+# load squad/roster data
 # user options: "mens" "womens" "mixed"
 load_rosters <- function(i) {
   file <- read_csv(curl(paste0("raw.githubusercontent.com/jbrooksdata/2022-olympic-curling/main/",i,"-tournament/player_info.csv")))
